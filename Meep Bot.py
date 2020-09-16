cbfa065f55e63abe3ef92ccecc8de7fbe6bbb614
Aetrjh <49579059+Aetrjh@users.noreply.github.com>
2020-09-15 20:34:14 -0400

    Add files via upload

--- Meep Bot.py
+++ Meep Bot.py
@@ -1,7 +1,11 @@
+#This imports the API key secrets from a hidden seperate file.
+import config
+
 import discord
 import os
 from discord.ext import commands
 
+#Sets the bot prefix.
 client = commands.Bot(command_prefix = "~")
 
 @client.command()
@@ -17,7 +21,10 @@ async def reload(ctx, extension):
     client.unload_extension(f"cogs.{extension}")
     client.load_extension(f"cogs.{extension}")
     
-
+#Loads each cog/extension.
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
+
+#Runs the bot with the hidden API key. You must set your own API key here for the bot to run.
+client.run(config.discordAPIKey)
